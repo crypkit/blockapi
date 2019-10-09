44afe03e5b2c0d166a5924a734d615832b58c489
onlineque <onlineque@seznam.cz>
2019-10-09 22:34:10 +0200

    new version set

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PACKAGES = find_packages(where='.')
 
 setuptools.setup(
     name='blockapi',
-    version='0.0.19',
+    version='0.0.20',
     author='Devmons s.r.o.',
     description='BlockAPI library',
     license='MIT',
