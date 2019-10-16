407cc9628a5aba389f82e7e552e2ed671eb7b994
Marek Galvánek <marek.galvanek@gmail.com>
2019-10-16 08:50:35 +0200

    change version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PACKAGES = find_packages(where='.')
 
 setuptools.setup(
     name='blockapi',
-    version='0.0.26',
+    version='0.0.27',
     author='Devmons s.r.o.',
     description='BlockAPI library',
     license='MIT',
