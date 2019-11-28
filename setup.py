fc77d95f447a0454ffb4d250875589151626acaf
mr_fuki <jozef.fuker@gmail.com>
2019-11-28 20:13:05 +0100

    Change version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PACKAGES = find_packages(where='.')
 
 setuptools.setup(
     name='blockapi',
-    version='0.0.44',
+    version='0.0.50',
     author='Devmons s.r.o.',
     description='BlockAPI library',
     license='MIT',
