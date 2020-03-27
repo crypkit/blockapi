8f91cbd3d8676c211f0b97be10dfdb602d98b301
Vladimir Siman <online@clew.cz>
2020-03-28 00:15:04 +0100

    etherscan - rate limit change

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PACKAGES = find_packages(where='.')
 
 setuptools.setup(
     name='blockapi',
-    version='0.0.46',
+    version='0.0.47',
     author='Devmons s.r.o.',
     description='BlockAPI library',
     license='MIT',
