3a73fd4619eae63297a69ffb538a740484f5da04
Vladimir Siman <online@clew.cz>
2020-05-04 21:09:04 +0200

    version updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PACKAGES = find_packages(where='.')
 
 setuptools.setup(
     name='blockapi',
-    version='0.0.62',
+    version='0.0.63',
     author='Devmons s.r.o.',
     description='BlockAPI library',
     license='MIT',
