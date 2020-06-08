6755f237a58dd36d1e1c1c4b15245713e190f6f0
Marek Galvánek <marek.galvanek@gmail.com>
2020-06-08 13:25:49 +0200

    new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PACKAGES = find_packages(where='.')
 
 setuptools.setup(
     name='blockapi',
-    version='0.0.64',
+    version='0.0.65',
     author='Devmons s.r.o.',
     description='BlockAPI library',
     license='MIT',
