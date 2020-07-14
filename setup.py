a207bd9a152da85150f44def89714378fb1dc66b
stepanVrtek <stepan.vrtek@gmail.com>
2020-07-14 12:28:13 +0200

    feat(release): new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PACKAGES = find_packages(where='.')
 
 setuptools.setup(
     name='blockapi',
-    version='0.0.70',
+    version='0.0.71',
     author='Devmons s.r.o.',
     description='BlockAPI library',
     license='MIT',
