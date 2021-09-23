2725dbbac4e6fc2950ee2885597a35abbf63b16a
marekgalvanek <marek.galvanek@gmail.com>
2021-09-23 12:53:57 +0200

    feat(requirements): delete unused gevent package

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import setuptools
-import setuptools
 from setuptools import find_packages
 
 with open("README.md", "r") as f:
@@ -9,7 +8,7 @@ PACKAGES = find_packages(where='.')
 
 setuptools.setup(
     name='blockapi',
-    version='0.0.83',
+    version='0.0.84',
     author='Devmons s.r.o.',
     description='BlockAPI library',
     license='MIT',
@@ -22,7 +21,6 @@ setuptools.setup(
         'python-dateutil>=2.8.0',
         'coinaddrng==1.0.30',
         'cfscrape>=2.0.8',
-        'gevent>=1.4.0',
         'ethereum_input_decoder>=0.2.2',
         'web3>=5.2.2',
         'bs4>=0.0.1',
