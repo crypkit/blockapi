588fde94cb0fc6e94a04a5bd7643640e62870bbb
Jan <janci.hanzlik+github@gmail.com>
2022-08-17 07:31:36 +0200

    Update version for release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PACKAGES = find_packages(where='.')
 
 setuptools.setup(
     name='blockapi',
-    version='0.1.16',
+    version='0.1.17',
     author='Devmons s.r.o.',
     description='BlockAPI library',
     license='MIT',
