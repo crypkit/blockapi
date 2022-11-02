00b46aeffe060cf5945adc9520642d27e3924b16
hanjano <janci.hanzlik+github@gmail.com>
2022-11-02 14:22:36 +0100

    feat: Add fetch limiter

---
+++ blockapi/test/v2/test_limiter.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timedelta
+
+import pytest
+from pytz import UTC
+
+from blockapi.v2.base import RateLimiter
+
+
+def test_use_different_wait(wait_capture, wait_func):
+    limiter = RateLimiter(wait_func)
+    limiter.wait(100)
+    assert wait_capture.seconds == [100]
+
+
+def test_limit_url_doesnt_limit_first_call(wait_capture, wait_func):
+    limiter = RateLimiter(wait_func)
+    limiter.set_rate("http://localhost:1234/", 0.5)
+    limiter.limit_url("http://localhost:1234/")
+    assert wait_capture.seconds == []
+
+
+def test_limit_url_limits_repeated_calls(wait_capture, wait_func):
+    limiter = RateLimiter(wait_func)
+    limiter.set_rate("http://localhost:1234/", 0.5)
+    limiter.limit_url("http://localhost:1234/")
+    limiter.limit_url("http://localhost:1234/")
+    assert 0.49 < wait_capture.seconds[0] < 0.5
+
+
+def test_limit_url_with_retry_after(wait_capture, wait_func):
+    limiter = RateLimiter(wait_func)
+    limiter.set_rate("http://localhost:1234/", 1)
+    limiter.limit_url("http://localhost:1234/")
+    limiter.limit_url("http://localhost:1234/", retry_after='900')
+    assert 899 < wait_capture.seconds[0] < 900
+
+
+def test_time_from_retry_after_seconds():
+    limit_low = datetime.utcnow() + timedelta(seconds=900)
+    limit_high = datetime.utcnow() + timedelta(seconds=901)
+
+    time = RateLimiter.time_from_retry_after('900')
+    assert limit_low <= time <= limit_high
+
+
+def test_time_from_retry_after_time():
+    time = RateLimiter.time_from_retry_after('Wed, 21 Oct 2015 07:28:00 CET')
+    assert time == datetime(2015, 10, 21, 5, 28, tzinfo=UTC)
+
+
+@pytest.fixture
+def wait_capture():
+    return WaitCapture()
+
+
+@pytest.fixture
+def wait_func(wait_capture):
+    def wait(seconds: int):
+        wait_capture.add(seconds)
+
+    return wait
+
+
+class WaitCapture:
+    def __init__(self):
+        self.seconds = []
+
+    def add(self, value):
+        self.seconds.append(value)
--- blockapi/v2/base.py
+++ blockapi/v2/base.py
@@ -1,10 +1,64 @@
 from abc import ABC
-from typing import Dict, List, Optional
-from urllib.parse import urljoin
+from datetime import datetime, timedelta
+from time import sleep as sync_sleep
+from typing import Callable, Dict, List, Optional
+from urllib.parse import urljoin, urlparse
 
+from dateutil import parser as date_parser
+from pytz import UTC
 from requests import HTTPError, Response, Session
 
 from .models import ApiOptions, BalanceItem, Coin, Pool
+
+
+class RateLimiter:
+    def __init__(self, wait_func: Callable):
+        self._timeouts = {}
+        self._rates = {}
+        self._wait_func = wait_func
+
+    def set_rate(self, url: str, rate: float):
+        self._rates[self._get_key(url)] = rate
+
+    def set_next_run(self, url: str, time: datetime):
+        self._timeouts[self._get_key(url)] = time
+
+    def limit_url(self, url, retry_after: Optional[str] = None):
+        key = self._get_key(url)
+
+        if retry_after:
+            self._timeouts[key] = self.time_from_retry_after(retry_after)
+
+        time = self._timeouts.get(key)
+        if time:
+            self.wait_until(time)
+
+        rate = self._rates.get(key)
+        if rate:
+            time = datetime.utcnow() + timedelta(seconds=rate)
+            self._timeouts[key] = time
+
+    def wait_until(self, time):
+        delay = (time - datetime.utcnow()).total_seconds()
+        if delay > 0:
+            self.wait(delay)
+
+    def wait(self, seconds: float):
+        self._wait_func(seconds)
+
+    @staticmethod
+    def time_from_retry_after(value: str):
+        try:
+            return datetime.utcnow() + timedelta(seconds=int(value))
+        except ValueError:
+            return date_parser.parse(value).astimezone(tz=UTC)
+
+    @staticmethod
+    def _get_key(url: str) -> str:
+        return urlparse(url).netloc
+
+
+rate_limiter = RateLimiter(sync_sleep)
 
 
 class BlockchainApi(ABC):
@@ -22,6 +76,9 @@ class BlockchainApi(ABC):
         self.api_key = api_key
         self._session = Session()
 
+        for url in self.supported_requests:
+            rate_limiter.set_rate(url, self.api_options.rate_limit)
+
     def __del__(self):
         self._session.close()
 
@@ -35,15 +92,15 @@ class BlockchainApi(ABC):
         Call specific request method with params and return raw response.
         """
         url = self._build_request_url(request_method, **req_args)
+        rate_limiter.limit_url(url)
         response = self._session.get(url, headers=headers)
+
+        if response.status_code == 429:
+            retry_after = response.headers['Retry-After']
+            rate_limiter.limit_url(url, retry_after=retry_after)
+            response = self._session.get(url, headers=headers)
+
         return self._check_and_get_from_response(response)
-
-    def _build_request_url(self, request_method: str, **req_args):
-        path_url = self.supported_requests.get(request_method)
-        if path_url is not None:
-            path_url = path_url.format(**req_args)
-
-        return urljoin(self.api_options.base_url, path_url)
 
     def post(self, request_method=None, body=None, json=None, headers=None):
         """
@@ -54,8 +111,22 @@ class BlockchainApi(ABC):
             if request_method
             else self.api_options.base_url
         )
+        rate_limiter.limit_url(url)
         response = self._session.post(url, data=body, json=json, headers=headers)
+
+        if response.status_code == 429:
+            retry_after = response.headers['Retry-After']
+            rate_limiter.limit_url(url, retry_after=retry_after)
+            response = self._session.post(url, data=body, json=json, headers=headers)
+
         return self._check_and_get_from_response(response)
+
+    def _build_request_url(self, request_method: str, **req_args):
+        path_url = self.supported_requests.get(request_method)
+        if path_url is not None:
+            path_url = path_url.format(**req_args)
+
+        return urljoin(self.api_options.base_url, path_url)
 
     def _check_and_get_from_response(self, response: Response) -> Dict:
         if response.status_code != 200:
