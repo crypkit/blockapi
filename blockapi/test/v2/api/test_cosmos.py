131efe122b2c972bc0173bf90a9f0811c3fdfc11
Vladimir Svoboda <ladovina@seznam.cz>
2024-01-17 13:47:39 +0100

    feat: refactoring: split token map loading into a separate class

--- blockapi/test/v2/api/test_cosmos.py
+++ blockapi/test/v2/api/test_cosmos.py
@@ -1,12 +1,18 @@
 import pytest
 
-from blockapi.v2.api.cosmos import CosmosApi
+from blockapi.v2.api.cosmos import (
+    CosmosApi,
+    CosmosCelestiaApi,
+    CosmosDydxApi,
+    CosmosOsmosisApi,
+    CosmosTokenMapLoader,
+)
 from blockapi.v2.models import UNKNOWN
 
 
 @pytest.fixture()
-def cosmos_api():
-    return CosmosApi()
+def token_loader():
+    return CosmosTokenMapLoader()
 
 
 @pytest.fixture()
@@ -43,16 +49,20 @@ def mocked_native_token_response():
     }
 
 
-def test_parse_native_tokens(cosmos_api, mocked_native_token_response, requests_mock):
+def test_parse_native_tokens(token_loader, mocked_native_token_response, requests_mock):
     requests_mock.get(
         f'https://raw.githubusercontent.com/PulsarDefi/IBC-Token-Data-Cosmos/main/native_token_data.min.json',
         json=mocked_native_token_response,
     )
 
-    cosmos_api.parse_native_tokens()
-    assert cosmos_api.tokens_map
-    assert cosmos_api.tokens_map['cosmoshub']
-    assert cosmos_api.tokens_map['dydx']
+    token_loader.parse_native_tokens()
+    assert token_loader.tokens_map
+
+    assert token_loader.tokens_map['cosmoshub']
+    assert "uatom" in token_loader.tokens_map['cosmoshub']
+
+    assert token_loader.tokens_map['dydx']
+    assert "adydx" in token_loader.tokens_map['dydx']
 
 
 @pytest.fixture()
@@ -76,45 +86,91 @@ def mocked_ibc_response():
 
 
 def test_parse_ibc_tokens(
-    cosmos_api, mocked_native_token_response, mocked_ibc_response, requests_mock
+    token_loader, mocked_native_token_response, mocked_ibc_response, requests_mock
 ):
     requests_mock.get(
-        cosmos_api.NATIVE_TOKEN_DATA_JSON,
+        token_loader.NATIVE_TOKEN_DATA_JSON,
         json=mocked_native_token_response,
     )
 
     requests_mock.get(
-        cosmos_api.IBC_DATA_JSON,
+        token_loader.IBC_DATA_JSON,
         json=mocked_ibc_response,
     )
 
-    assert cosmos_api.tokens_map
+    assert token_loader.tokens_map
     assert (
-        cosmos_api.tokens_map["akash"][
+        token_loader.tokens_map["akash"][
             "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2"
         ]
-        == cosmos_api.tokens_map["cosmoshub"]["uatom"]
+        == token_loader.tokens_map["cosmoshub"]["uatom"]
     )
     assert (
-        cosmos_api.tokens_map["osmosis"][
+        token_loader.tokens_map["osmosis"][
             "ibc/276EB8E30E8E1673FFDC80DBC79BF864AD83888F455BE970ED06ED5E13A9BEA6"
         ]
-        == cosmos_api.tokens_map["dydx"]["adydx"]
+        == token_loader.tokens_map["dydx"]["adydx"]
     )
 
 
-def test_parse_ibc_tokens_no_native(cosmos_api, mocked_ibc_response, requests_mock):
+@pytest.fixture()
+def mocked_ibc_response_cosmoshub_only():
+    return {
+        "ibc/00255B18FBBC1E36845AAFDCB4CBD460DC45331496A64C2A29CEAFDD3B997B5F__cosmoshub": {
+            "chain": "cosmoshub",
+            "hash": "00255B18FBBC1E36845AAFDCB4CBD460DC45331496A64C2A29CEAFDD3B997B5F",
+            "supply": "3015000000000000000000",
+            "path": "transfer/channel-281",
+            "origin": {
+                "denom": "adydx",
+                "chain": "dydx",
+            },
+        }
+    }
+
+
+def test_parse_ibc_tokens_2(
+    token_loader,
+    mocked_native_token_response,
+    mocked_ibc_response_cosmoshub_only,
+    requests_mock,
+):
+    token_loader.filter_by_chain = "cosmoshub"
     requests_mock.get(
-        cosmos_api.NATIVE_TOKEN_DATA_JSON,
+        token_loader.NATIVE_TOKEN_DATA_JSON,
+        json=mocked_native_token_response,
+    )
+    requests_mock.get(
+        token_loader.IBC_DATA_JSON,
+        json=mocked_ibc_response_cosmoshub_only,
+    )
+
+    assert token_loader.tokens_map
+
+    assert "cosmoshub" in token_loader.tokens_map
+
+    # tokens_map contain native tokens
+    assert "uatom" in token_loader.tokens_map["cosmoshub"]
+    assert "adydx" not in token_loader.tokens_map["cosmoshub"]
+
+    assert (
+        "ibc/00255B18FBBC1E36845AAFDCB4CBD460DC45331496A64C2A29CEAFDD3B997B5F"
+        in token_loader.tokens_map["cosmoshub"]
+    )
+
+
+def test_parse_ibc_tokens_no_native(token_loader, mocked_ibc_response, requests_mock):
+    requests_mock.get(
+        token_loader.NATIVE_TOKEN_DATA_JSON,
         json={},
     )
 
     requests_mock.get(
-        cosmos_api.IBC_DATA_JSON,
+        token_loader.IBC_DATA_JSON,
         json=mocked_ibc_response,
     )
 
-    assert cosmos_api.tokens_map
+    assert token_loader.tokens_map
 
 
 @pytest.fixture()
@@ -131,33 +187,60 @@ def mocked_ibc_response_multiple_chains():
 
 
 def test_parse_ibc_tokens_multiple_chains(
-    cosmos_api,
+    token_loader,
     mocked_native_token_response,
     mocked_ibc_response_multiple_chains,
     requests_mock,
 ):
     requests_mock.get(
-        cosmos_api.NATIVE_TOKEN_DATA_JSON,
+        token_loader.NATIVE_TOKEN_DATA_JSON,
         json=mocked_native_token_response,
     )
 
     requests_mock.get(
-        cosmos_api.IBC_DATA_JSON,
+        token_loader.IBC_DATA_JSON,
         json=mocked_ibc_response_multiple_chains,
     )
 
-    assert cosmos_api.tokens_map
+    assert token_loader.tokens_map
     assert (
-        cosmos_api.tokens_map['akash'][
+        token_loader.tokens_map['akash'][
             'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2'
         ]
-        == cosmos_api.tokens_map['cosmoshub']['uatom']
+        == token_loader.tokens_map['cosmoshub']['uatom']
     )
 
 
 @pytest.mark.vcr()
 @pytest.mark.integration
-def test_get_balance(cosmos_api):
-    balances = cosmos_api.get_balance('cosmos14s5zljjqtzyl2zfey7ytkllyp0tampkel24qyu')
+def test_cosmos_get_balance():
+    balances = CosmosApi().get_balance('cosmos14s5zljjqtzyl2zfey7ytkllyp0tampkel24qyu')
     assert len(balances) == 17
+    assert all([balance.coin != UNKNOWN for balance in balances])
+
+
+@pytest.mark.vcr()
+@pytest.mark.integration
+def test_osmosis_get_balance():
+    api = CosmosOsmosisApi()
+    balances = api.get_balance('osmo1aff76avnwpnk02wxkc6n5xnwasjkgekazvl8xj')
+    assert len(balances) == 2
+    assert all([balance.coin != UNKNOWN for balance in balances])
+
+
+@pytest.mark.vcr()
+@pytest.mark.integration
+def test_dydx_get_balance():
+    api = CosmosDydxApi()
+    balances = api.get_balance('dydx1aff76avnwpnk02wxkc6n5xnwasjkgekarwznsh')
+    assert len(balances) == 4
+    assert all([balance.coin != UNKNOWN for balance in balances])
+
+
+@pytest.mark.vcr()
+@pytest.mark.integration
+def test_celestia_get_balance():
+    api = CosmosCelestiaApi()
+    balances = api.get_balance('celestia1aff76avnwpnk02wxkc6n5xnwasjkgekamaa82d')
+    assert len(balances) == 1
     assert all([balance.coin != UNKNOWN for balance in balances])
--- blockapi/v2/api/cosmos.py
+++ blockapi/v2/api/cosmos.py
@@ -2,7 +2,9 @@ import logging
 from abc import ABCMeta
 from collections import defaultdict
 from decimal import Decimal
-from typing import Iterable, Optional
+from typing import Iterable, Optional, Union
+
+from requests import Session
 
 from blockapi.utils.num import to_decimal
 from blockapi.v2.base import ApiOptions, BlockchainApi, IBalance
@@ -12,40 +14,16 @@ from blockapi.v2.models import AssetType, BalanceItem, Blockchain, Coin
 logger = logging.getLogger(__name__)
 
 
-class CosmosApiBase(BlockchainApi, IBalance, metaclass=ABCMeta):
-    """
-    Cosmos RPC
-    API docs: https://cosmos.network/rpc/
-    Explorer: https://www.mintscan.io
-    """
-
-    coin = COIN_ATOM
-
-    API_BASE_URL = 'https://lcd-cosmos.cosmostation.io/'
-    API_BASE_RATE_LIMIT = 0.2
-
-    supported_requests = {
-        'get_balances': '/cosmos/bank/v1beta1/balances/{address}',
-        'get_staked_balance': (
-            '/cosmos/staking/v1beta1/delegations/{address}?pagination.limit=1000'
-        ),
-        'get_unbonding_balance': (
-            '/cosmos/staking/v1beta1/delegators/{address}/unbonding_delegations'
-        ),
-        'get_rewards': 'cosmos/distribution/v1beta1/delegators/{address}/rewards',
-        # TODO support commissions, valid only for validators?
-        'get_commission': (
-            'cosmos/distribution/v1beta1/validators/{validator_address}/commission'
-        ),
-    }
-
+class CosmosTokenMapLoader:
     NATIVE_TOKEN_DATA_JSON = 'https://raw.githubusercontent.com/PulsarDefi/IBC-Token-Data-Cosmos/main/native_token_data.min.json'
     IBC_DATA_JSON = 'https://raw.githubusercontent.com/PulsarDefi/IBC-Token-Data-Cosmos/main/ibc_data.min.json'
 
-    _tokens_map: Optional[dict[str, dict]] = defaultdict(dict)
+    def __init__(self):
+        self._session = Session()
+        self._tokens_map = defaultdict(dict)
 
     @property
-    def tokens_map(self) -> dict[str, dict]:
+    def tokens_map(self) -> Union[defaultdict[str, dict], dict[str, dict]]:
         if not self._tokens_map:
             self.parse_native_tokens()
             self.parse_ibc_data()
@@ -79,7 +57,7 @@ class CosmosApiBase(BlockchainApi, IBalance, metaclass=ABCMeta):
             native_token_denom = value["origin"]["denom"]
 
             if native_token_denom not in self._tokens_map[native_token_chain]:
-                logger.debug(
+                logger.warning(
                     f"Skipping IBC {ibc_denom}, no native token: {native_token_denom}, chain: {native_token_chain}."
                 )
                 continue
@@ -90,9 +68,51 @@ class CosmosApiBase(BlockchainApi, IBalance, metaclass=ABCMeta):
 
             self._tokens_map[chain][ibc_denom] = native_token_value
 
+
+class CosmosApiBase(BlockchainApi, IBalance, metaclass=ABCMeta):
+    """
+    Cosmos RPC
+    API docs: https://cosmos.network/rpc/
+    Explorer: https://www.mintscan.io
+    """
+
+    coin = COIN_ATOM
+
+    API_BASE_URL = 'https://lcd-cosmos.cosmostation.io/'
+    API_BASE_RATE_LIMIT = 0.2
+
+    supported_requests = {
+        'get_balances': '/cosmos/bank/v1beta1/balances/{address}',
+        'get_staked_balance': (
+            '/cosmos/staking/v1beta1/delegations/{address}?pagination.limit=1000'
+        ),
+        'get_unbonding_balance': (
+            '/cosmos/staking/v1beta1/delegators/{address}/unbonding_delegations'
+        ),
+        'get_rewards': 'cosmos/distribution/v1beta1/delegators/{address}/rewards',
+        # TODO support commissions, valid only for validators?
+        'get_commission': (
+            'cosmos/distribution/v1beta1/validators/{validator_address}/commission'
+        ),
+    }
+
+    TOKENS_MAP_BLOCKCHAIN_KEY = None
+
+    def __init__(
+        self, tokens_map: defaultdict[str, dict] = None, api_key: Optional[str] = None
+    ):
+        super().__init__()
+        self._tokens_map = tokens_map
+
+    @property
+    def blockchain_tokens_map(self) -> dict[str, dict]:
+        if not self._tokens_map:
+            self._tokens_map = CosmosTokenMapLoader().tokens_map
+
+        return self._tokens_map[self.TOKENS_MAP_BLOCKCHAIN_KEY]
+
     def get_balance(self, address: str) -> list[BalanceItem]:
         balances = []
-
         balances.extend(list(self._yield_available_balance(address)))
         if staked_balance := self._get_staked_balance(address):
             balances.append(staked_balance)
@@ -101,9 +121,7 @@ class CosmosApiBase(BlockchainApi, IBalance, metaclass=ABCMeta):
 
         return balances
 
-    def _yield_available_balance(
-        self, address: str, chain: str = 'cosmoshub'
-    ) -> Iterable[BalanceItem]:
+    def _yield_available_balance(self, address: str) -> Iterable[BalanceItem]:
         response = self._get('get_balances', address=address)
 
         for b in response['balances']:
@@ -112,8 +130,8 @@ class CosmosApiBase(BlockchainApi, IBalance, metaclass=ABCMeta):
                     balance_raw=b['amount'], coin=self.coin, raw=b
                 )
             else:
-                if b['denom'] in self.tokens_map[chain]:
-                    token = self._get_token_data(chain, b['denom'])
+                if b['denom'] in self.blockchain_tokens_map:
+                    token = self._get_token_data(b['denom'])
                 else:
                     token = Coin.from_api(
                         blockchain=Blockchain.COSMOS,
@@ -122,8 +140,8 @@ class CosmosApiBase(BlockchainApi, IBalance, metaclass=ABCMeta):
                     )
                 yield BalanceItem.from_api(balance_raw=b['amount'], coin=token, raw=b)
 
-    def _get_token_data(self, chain: str, denom: str) -> Coin:
-        raw_token = self.tokens_map[chain][denom]
+    def _get_token_data(self, denom: str) -> Coin:
+        raw_token = self.blockchain_tokens_map[denom]
         return Coin(
             symbol=raw_token['symbol'],
             name=raw_token['name'],
@@ -171,21 +189,13 @@ class CosmosApiBase(BlockchainApi, IBalance, metaclass=ABCMeta):
 
         rewards = []
         for reward in response['total']:
-            chain_tokens = self.tokens_map[chain]
-
-            if not chain_tokens:
-                logger.warning(
-                    f"Skipping rewards, chain {chain} has no token mappings."
-                )
-                return []
-
-            if reward['denom'] not in chain_tokens:
+            if reward['denom'] not in self.blockchain_tokens_map:
                 logger.warning(
                     f"Skipping reward: {reward['denom']} no matching token found."
                 )
                 continue
 
-            token = self._get_token_data(chain, reward['denom'])
+            token = self._get_token_data(reward['denom'])
             balance_reward = BalanceItem.from_api(
                 balance_raw=reward['amount'],
                 coin=token,
@@ -212,6 +222,7 @@ class CosmosApiBase(BlockchainApi, IBalance, metaclass=ABCMeta):
 
 class CosmosApi(CosmosApiBase):
     coin = COIN_ATOM
+    TOKENS_MAP_BLOCKCHAIN_KEY = "cosmoshub"
     api_options = ApiOptions(
         blockchain=Blockchain.COSMOS,
         base_url=CosmosApiBase.API_BASE_URL,
@@ -221,6 +232,7 @@ class CosmosApi(CosmosApiBase):
 
 class CosmosOsmosisApi(CosmosApiBase):
     coin = COIN_DYDX
+    TOKENS_MAP_BLOCKCHAIN_KEY = "osmosis"
     api_options = ApiOptions(
         blockchain=Blockchain.COSMOS,
         base_url='https://lcd-osmosis.cosmostation.io/',
@@ -230,6 +242,7 @@ class CosmosOsmosisApi(CosmosApiBase):
 
 class CosmosDydxApi(CosmosApiBase):
     coin = COIN_DYDX
+    TOKENS_MAP_BLOCKCHAIN_KEY = "dydx"
     api_options = ApiOptions(
         blockchain=Blockchain.COSMOS,
         base_url='https://lcd-dydx.cosmostation.io/',
@@ -239,6 +252,7 @@ class CosmosDydxApi(CosmosApiBase):
 
 class CosmosCelestiaApi(CosmosApiBase):
     coin = COIN_CELESTIA
+    TOKENS_MAP_BLOCKCHAIN_KEY = "celestia"
     api_options = ApiOptions(
         blockchain=Blockchain.COSMOS,
         base_url='https://lcd-celestia.cosmostation.io/',
