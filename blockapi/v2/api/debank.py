6b1d20dc3cb77f1db0cad058a5068d2c7c0ab1b0
hanjano <janci.hanzlik+github@gmail.com>
2025-02-04 12:52:53 +0100

    fix: ignore duplicate symbols in asset import list

--- blockapi/v2/api/debank.py
+++ blockapi/v2/api/debank.py
@@ -484,32 +484,56 @@ class DebankPortfolioParser:
         asset_type = self._parse_asset_type(item.name)
         borrow_type = self._get_borrow_asset_type(asset_type)
         reward_type = self._get_reward_asset_type(asset_type)
+        exclude = set()
 
-        yield from self._parse_token_list(
-            detail.supply_token_list,
-            asset_type,
-            pool_info=pool_info,
+        supply = list(
+            self._parse_token_list(
+                detail.supply_token_list,
+                asset_type,
+                pool_info=pool_info,
+            )
         )
+        self._update_exclude(exclude, supply)
+        yield from supply
 
-        yield from self._parse_token_list(
-            detail.borrow_token_list,
-            borrow_type,
-            pool_info=pool_info,
+        borrow = list(
+            self._parse_token_list(
+                detail.borrow_token_list,
+                borrow_type,
+                pool_info=pool_info,
+            )
         )
+        self._update_exclude(exclude, borrow)
+        yield from borrow
 
-        yield from self._parse_token_list(
-            detail.reward_token_list,
-            reward_type,
-            pool_info=pool_info,
+        reward = list(
+            self._parse_token_list(
+                detail.reward_token_list,
+                reward_type,
+                pool_info=pool_info,
+            )
         )
+        self._update_exclude(exclude, reward)
+        yield from reward
 
-        yield from self._parse_token_list(
-            detail.token_list, asset_type, pool_info=pool_info
+        tokens = list(
+            self._parse_token_list(detail.token_list, asset_type, pool_info=pool_info)
         )
+        # self._update_exclude(exclude, tokens)
+        yield from tokens
 
-        yield from self._parse_token_list(
+        assets = self._parse_token_list(
             item.asset_token_list, asset_type, pool_info=pool_info
         )
+
+        for asset in assets:
+            if (asset.coin.blockchain, asset.coin.symbol) not in exclude:
+                yield asset
+
+    @staticmethod
+    def _update_exclude(exclude: set, items: list[BalanceItem]):
+        for it in items:
+            exclude.add((it.coin.blockchain, it.coin.symbol))
 
     def _get_tokens(self, raw_balances: list[dict]):
         symbols = [
