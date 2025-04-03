22f263f24422eef0f434af1056127824283c0edb
CoCoNuTeK <sefcik.simon@gmail.com>
2025-04-03 14:18:54 +0200

    fix: to pass tests

--- blockapi/v2/api/nft/unisat.py
+++ blockapi/v2/api/nft/unisat.py
@@ -203,7 +203,7 @@ class UnisatApi(BlockchainApi, INftParser, INftProvider):
         )
 
         return FetchResult.from_fetch_results(
-            [info_response, items_response, stats_response]
+            info=info_response, items=items_response, stats=stats_response
         )
 
     def parse_collection(self, fetch_result: FetchResult) -> ParseResult:
